512027eaa50a18a4eebe14686e97ba618b9b451a
pedrotejero <pedrotejero12@gmail.com>
2025-04-27 17:57:45 +0200

    Outside static-variables definition

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,16 +1,4 @@
 """ This is a dummy example """
-import numpy as np
-import requests
-
-
-API_URL = "<https://archive-api.open-meteo.com/v1/archive?>"
-
-COORDINATES = {
- "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
- "London": {"latitude": 51.507351, "longitude": -0.127758},
- "Rio": {"latitude": -22.906847, "longitude": -43.172896},
-}
-VARIABLES = ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max"]
 
 
 def main():
